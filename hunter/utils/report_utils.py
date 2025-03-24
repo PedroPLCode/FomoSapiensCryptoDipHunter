9705a60f4d81169d235a624d4b14b8a03beff76e
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2025-03-24 09:30:37 +0100

    Change telegram msgs layout

--- hunter/utils/report_utils.py
+++ hunter/utils/report_utils.py
@@ -40,12 +40,14 @@ def generate_hunter_signal_content(
 
     content = (
         f"FomoSapiensCryptoDipHunter\n"
+        f"https://fomo.ropeaccess.pro\n\n"
         f"Current {signal.upper()} signal.\n"
         f"{formatted_now}\n\n"
         f"Hunter {hunter.id} {hunter.symbol}\n"
         f"interval: {hunter.interval}\n"
         f"lookback: {hunter.lookback}\n"
-        f"comment: {hunter.comment}\n\n"
+        f"comment: {hunter.comment}\n"
+        f"note: {hunter.note}\n\n"
         f"Hunter details:\n\n"
         f"open_time: {latest_data['open_time']}\n"
         f"close_time: {latest_data['close_time']}\n\n"
@@ -59,12 +61,12 @@ def generate_hunter_signal_content(
         f"avg_volume_period: {hunter.avg_volume_period}\n"
         f"volume_latest_data: {latest_data['volume']}\n"
         f"volume_previous_data: {previous_data['volume']}\n"
-        f"avg_volume: {averages['avg_volume']}\n\n"
+        f"avg_volume: {averages['avg_volume']}"
     )
 
     if hunter.rsi_signals or hunter.rsi_divergence_signals:
         content += (
-            "RSI (Relative Strength Index):\n"
+            "\n\nRSI (Relative Strength Index):\n"
             f"rsi_signals: {hunter.rsi_signals}\n"
             f"rsi_divergence_signals: {hunter.rsi_divergence_signals}\n"
             f"rsi_timeperiod: {hunter.rsi_timeperiod}\n"
@@ -73,12 +75,12 @@ def generate_hunter_signal_content(
             f"rsi_previous_data: {previous_data['rsi']}\n"
             f"avg_rsi: {averages['avg_rsi']}\n"
             f"rsi_buy: {hunter.rsi_buy}\n"
-            f"rsi_sell: {hunter.rsi_sell}\n\n"
+            f"rsi_sell: {hunter.rsi_sell}"
         )
 
     if hunter.cci_signals or hunter.cci_divergence_signals:
         content += (
-            "CCI (Commodity Channel Index):\n"
+            "\n\nCCI (Commodity Channel Index):\n"
             f"cci_signals: {hunter.cci_signals}\n"
             f"cci_divergence_signals: {hunter.cci_divergence_signals}\n"
             f"cci_timeperiod: {hunter.cci_timeperiod}\n"
@@ -87,12 +89,12 @@ def generate_hunter_signal_content(
             f"cci_previous_data: {previous_data['cci']}\n"
             f"avg_cci: {averages['avg_cci']}\n"
             f"cci_buy: {hunter.cci_buy}\n"
-            f"cci_sell: {hunter.cci_sell}\n\n"
+            f"cci_sell: {hunter.cci_sell}"
         )
 
     if hunter.mfi_signals or hunter.mfi_divergence_signals:
         content += (
-            "MFI (Money Flow Index):\n"
+            "\n\nMFI (Money Flow Index):\n"
             f"mfi_signals: {hunter.mfi_signals}\n"
             f"mfi_divergence_signals: {hunter.mfi_divergence_signals}\n"
             f"mfi_timeperiod: {hunter.mfi_timeperiod}\n"
@@ -101,12 +103,12 @@ def generate_hunter_signal_content(
             f"mfi_previous_data: {previous_data['mfi']}\n"
             f"avg_mfi: {averages['avg_mfi']}\n"
             f"mfi_buy: {hunter.mfi_buy}\n"
-            f"mfi_sell: {hunter.mfi_sell}\n\n"
+            f"mfi_sell: {hunter.mfi_sell}"
         )
 
     if hunter.macd_cross_signals or hunter.macd_histogram_signals:
         content += (
-            "MACD (Moving Average Convergence Divergence):\n"
+            "\n\nMACD (Moving Average Convergence Divergence):\n"
             f"macd_cross_signals: {hunter.macd_cross_signals}\n"
             f"macd_histogram_signals: {hunter.macd_histogram_signals}\n"
             f"macd_timeperiod: {hunter.macd_timeperiod}\n"
@@ -117,12 +119,12 @@ def generate_hunter_signal_content(
             f"macd_previous_data: {previous_data['macd']}\n"
             f"macd_signal_previous_data: {previous_data['macd_signal']}\n"
             f"avg_macd: {averages['avg_macd']}\n"
-            f"avg_macd_signal: {averages['avg_macd_signal']}\n\n"
+            f"avg_macd_signal: {averages['avg_macd_signal']}"
         )
 
     if hunter.bollinger_signals:
         content += (
-            "Bollinger Bands:\n"
+            "\n\nBollinger Bands:\n"
             f"bollinger_signals: {hunter.bollinger_signals}\n"
             f"bollinger_timeperiod: {hunter.bollinger_timeperiod}\n"
             f"bollinger_nbdev: {hunter.bollinger_nbdev}\n"
@@ -131,12 +133,12 @@ def generate_hunter_signal_content(
             f"lower_band_latest_data: {latest_data['lower_band']}\n"
             f"upper_band_previous_data: {previous_data['upper_band']}\n"
             f"middle_band_previous_data: {previous_data['middle_band']}\n"
-            f"lower_band_previous_data: {previous_data['lower_band']}\n\n"
+            f"lower_band_previous_data: {previous_data['lower_band']}"
         )
 
     if hunter.stoch_signals or hunter.stoch_divergence_signals:
         content += (
-            "Stochastic Indicators:\n"
+            "\n\nStochastic Indicators:\n"
             f"stoch_signals: {hunter.stoch_signals}\n"
             f"stoch_divergence_signals: {hunter.stoch_divergence_signals}\n"
             f"stoch_k_timeperiod: {hunter.stoch_k_timeperiod}\n"
@@ -149,12 +151,12 @@ def generate_hunter_signal_content(
             f"avg_stoch_k: {averages['avg_stoch_k']}\n"
             f"avg_stoch_d: {averages['avg_stoch_d']}\n"
             f"stoch_buy: {hunter.stoch_buy}\n"
-            f"stoch_sell: {hunter.stoch_sell}\n\n"
+            f"stoch_sell: {hunter.stoch_sell}"
         )
 
     if hunter.stoch_rsi_signals:
         content += (
-            "Stochastic RSI Indicators:\n"
+            "\n\nStochastic RSI Indicators:\n"
             f"stoch_rsi_signals: {hunter.stoch_rsi_signals}\n"
             f"stoch_rsi_timeperiod: {hunter.stoch_rsi_timeperiod}\n"
             f"stoch_rsi_k_timeperiod: {hunter.stoch_rsi_k_timeperiod}\n"
@@ -165,12 +167,12 @@ def generate_hunter_signal_content(
             f"stoch_rsi_d_previous_data: {previous_data['stoch_rsi_d']}\n"
             f"stoch_rsi_k_previous_data: {previous_data['stoch_rsi_k']}\n"
             f"avg_stoch_rsi_d: {averages['avg_stoch_rsi_d']}\n"
-            f"avg_stoch_rsi_k: {averages['avg_stoch_rsi_k']}\n\n"
+            f"avg_stoch_rsi_k: {averages['avg_stoch_rsi_k']}"
         )
 
     if hunter.ema_cross_signals or hunter.ema_fast_signals or hunter.ema_slow_signals:
         content += (
-            "EMA (Exponential Moving Averages):\n"
+            "\n\nEMA (Exponential Moving Averages):\n"
             f"ema_cross_signals: {hunter.ema_cross_signals}\n"
             f"ema_fast_signals: {hunter.ema_fast_signals}\n"
             f"ema_slow_signals: {hunter.ema_slow_signals}\n"
@@ -182,12 +184,12 @@ def generate_hunter_signal_content(
             f"ema_fast_previous_data: {previous_data['ema_fast']}\n"
             f"ema_slow_previous_data: {previous_data['ema_slow']}\n"
             f"avg_ema_fast: {averages['avg_ema_fast']}\n"
-            f"avg_ema_slow: {averages['avg_ema_slow']}\n\n"
+            f"avg_ema_slow: {averages['avg_ema_slow']}"
         )
 
     if hunter.di_signals:
         content += (
-            "Directional Movement Index (DMI):\n"
+            "\n\nDirectional Movement Index (DMI):\n"
             f"di_signals: {hunter.di_signals}\n"
             f"di_timeperiod: {hunter.di_timeperiod}\n"
             f"avg_di_period: {hunter.avg_di_period}\n"
@@ -196,58 +198,58 @@ def generate_hunter_signal_content(
             f"plus_di_previous_data: {previous_data['plus_di']}\n"
             f"minus_di_previous_data: {previous_data['minus_di']}\n"
             f"avg_plus_di: {averages['avg_plus_di']}\n"
-            f"avg_minus_di: {averages['avg_minus_di']}\n\n"
+            f"avg_minus_di: {averages['avg_minus_di']}"
         )
 
     if hunter.atr_signals:
         content += (
-            "ATR (Average True Range):\n"
+            "\n\nATR (Average True Range):\n"
             f"atr_signals: {hunter.atr_signals}\n"
             f"atr_timeperiod: {hunter.atr_timeperiod}\n"
             f"avg_atr_period: {hunter.avg_atr_period}\n"
             f"atr_latest_data: {latest_data['atr']}\n"
             f"atr_previous_data: {previous_data['atr']}\n"
             f"avg_atr: {averages['avg_atr']}\n"
-            f"atr_buy_threshold: {hunter.atr_buy_threshold}\n\n"
+            f"atr_buy_threshold: {hunter.atr_buy_threshold}"
         )
 
     if hunter.vwap_signals:
         content += (
-            "VWAP (Volume Weighted Average Price):\n"
+            "\n\nVWAP (Volume Weighted Average Price):\n"
             f"vwap_signals: {hunter.vwap_signals}\n"
             f"avg_vwap_period: {hunter.avg_vwap_period}\n"
             f"vwap_latest_data: {latest_data['vwap']}\n"
             f"vwap_previous_data: {previous_data['vwap']}\n"
-            f"avg_vwap: {averages['avg_vwap']}\n\n"
+            f"avg_vwap: {averages['avg_vwap']}"
         )
 
     if hunter.psar_signals:
         content += (
-            "Parabolic SAR:\n"
+            "\n\nParabolic SAR:\n"
             f"psar_signals: {hunter.psar_signals}\n"
             f"psar_acceleration: {hunter.psar_acceleration}\n"
             f"psar_maximum: {hunter.psar_maximum}\n"
             f"avg_psar_period: {hunter.avg_psar_period}\n"
             f"psar_latest_data: {latest_data['psar']}\n"
             f"psar_previous_data: {previous_data['psar']}\n"
-            f"avg_psar: {averages['avg_psar']}\n\n"
+            f"avg_psar: {averages['avg_psar']}"
         )
 
     if hunter.ma50_signals or hunter.ma200_signals or hunter.ma_cross_signals:
         content += (
-            "Moving Averages:\n"
+            "\n\nMoving Averages:\n"
             f"ma50_signals: {hunter.ma50_signals}\n"
             f"ma200_signals: {hunter.ma200_signals}\n"
             f"ma_cross_signals: {hunter.ma_cross_signals}\n"
             f"ma_50_latest_data: {latest_data['ma_50']}\n"
             f"ma_200_latest_data: {latest_data['ma_200']}\n"
             f"ma_50_previous_data: {previous_data['ma_50']}\n"
-            f"ma_200_previous_data: {previous_data['ma_200']}\n\n"
+            f"ma_200_previous_data: {previous_data['ma_200']}"
         )
 
     if hunter.trend_signals:
         content += (
-            "ADX Trend:\n"
+            "\n\nADX Trend:\n"
             f"trend_signals: {hunter.trend_signals}\n"
             f"adx_timeperiod: {hunter.adx_timeperiod}\n"
             f"avg_adx_period: {hunter.avg_adx_period}\n"
